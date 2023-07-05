90b98d97131db9d5076e84cb8e9679a82f33ee17
Wyn Chrsitian <80365421+Wyn-Christian@users.noreply.github.com>
2023-07-05 10:30:39 +0800

    Create Nav component

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -8,49 +8,57 @@ import IconButton from "@mui/material/IconButton"; */
 import logo from "../assets/header-logo.png";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
-import { Box } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 
 /* import MenuIcon from "@mui/icons-material/Menu"; */
 
 export default function Nav() {
-  return (
-    <Box
-      sx={{
-        flexGrow: 1,
-      }}
-    >
-      <AppBar
-        position="sticky"
-        sx={{ backgroundColor: "#FFFFFF", paddingY: 1 }}
-      >
-        <Toolbar>
-          <MenuSelect
-            edge="start"
-            aria-label="menu"
-            sx={{ mr: 2, flexGrow: 1 }}
-          />
-          <Box
-            component="img"
-            sx={{
-              flexGrow: 3,
-              width: "15%",
-              aspectRatio: "auto",
-              objectFit: "contain",
-              height: 100,
-            }}
-            alt="Navbar Icon"
-            src={logo}
-          />
-          <Box sx={{ flexGrow: 1, flexDirection: "row" }}>
-            <Box sx={{ color: "primary.red" }}>
-              <AccountCircleOutlinedIcon fontSize="large" />
-            </Box>
-            <Box sx={{ color: "primary.red" }}>
-              <ShoppingCartOutlinedIcon fontSize="large" />
-            </Box>
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+	return (
+		<Box
+			sx={{
+				flexGrow: 1,
+			}}
+		>
+			<AppBar
+				position="sticky"
+				sx={{ backgroundColor: "#FFFFFF", paddingY: 1 }}
+			>
+				<Toolbar>
+					<MenuSelect
+						edge="start"
+						aria-label="menu"
+						sx={{ mr: 2, flexGrow: 1 }}
+					/>
+					<Box
+						component="img"
+						sx={{
+							flexGrow: 3,
+							width: "15%",
+							aspectRatio: "auto",
+							objectFit: "contain",
+							height: 100,
+						}}
+						alt="Navbar Icon"
+						src={logo}
+					/>
+					<Stack direction={{ xs: "column", md: "row" }} spacing={2}>
+						<Box sx={{ color: "primary.red" }}>
+							<AccountCircleOutlinedIcon
+								sx={{
+									fontSize: { xs: 25, md: 35 },
+								}}
+							/>
+						</Box>
+						<Box sx={{ color: "primary.red" }}>
+							<ShoppingCartOutlinedIcon
+								sx={{
+									fontSize: { xs: 25, md: 35 },
+								}}
+							/>
+						</Box>
+					</Stack>
+				</Toolbar>
+			</AppBar>
+		</Box>
+	);
 }
