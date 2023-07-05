abecb0d254c804feb380769ba561a285412e5e1e
Wyn Chrsitian <80365421+Wyn-Christian@users.noreply.github.com>
2023-07-05 10:30:19 +0800

    Create Menu Component

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -4,70 +4,75 @@ import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 
 export default function BasicMenu() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const open = Boolean(anchorEl);
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
 
-  return (
-    <div>
-      <Button
-        id="basic-button"
-        aria-controls={open ? "basic-menu" : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? "true" : undefined}
-        onClick={handleClick}
-        sx={{
-          color: "primary.red",
-        }}
-        size="large"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M4 6h16M4 12h16M4 18h7"
-          />
-        </svg>
-      </Button>
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          "aria-labelledby": "basic-button",
-        }}
-      >
-        <MenuItem
-          onClick={handleClose}
-          sx={{ backgroundColor: "primary.red", color: "primary.text" }}
-        >
-          Profile
-        </MenuItem>
-        <MenuItem
-          onClick={handleClose}
-          sx={{ backgroundColor: "primary.red", color: "primary.text" }}
-        >
-          My account
-        </MenuItem>
-        <MenuItem
-          onClick={handleClose}
-          sx={{ backgroundColor: "primary.red", color: "primary.text" }}
-        >
-          Logout
-        </MenuItem>
-      </Menu>
-    </div>
-  );
+	return (
+		<div>
+			<Button
+				id="basic-button"
+				aria-controls={open ? "basic-menu" : undefined}
+				aria-haspopup="true"
+				aria-expanded={open ? "true" : undefined}
+				onClick={handleClick}
+				sx={{
+					color: "primary.red",
+				}}
+				size="large"
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+				>
+					<path
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						strokeWidth="2"
+						d="M4 6h16M4 12h16M4 18h7"
+					/>
+				</svg>
+			</Button>
+			<Menu
+				id="basic-menu"
+				anchorEl={anchorEl}
+				open={open}
+				onClose={handleClose}
+				MenuListProps={{
+					"aria-labelledby": "basic-button",
+				}}
+				sx={{
+					"& .MuiList-padding": {
+						padding: 0,
+					},
+				}}
+			>
+				<MenuItem
+					onClick={handleClose}
+					sx={{ backgroundColor: "primary.red", color: "primary.text" }}
+				>
+					Profile
+				</MenuItem>
+				<MenuItem
+					onClick={handleClose}
+					sx={{ backgroundColor: "primary.red", color: "primary.text" }}
+				>
+					My account
+				</MenuItem>
+				<MenuItem
+					onClick={handleClose}
+					sx={{ backgroundColor: "primary.red", color: "primary.text" }}
+				>
+					Logout
+				</MenuItem>
+			</Menu>
+		</div>
+	);
 }
