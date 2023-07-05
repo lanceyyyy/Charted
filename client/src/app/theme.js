87f6650c7d284ad02ddb9504718a23d23f897c3e
Wyn Chrsitian <80365421+Wyn-Christian@users.noreply.github.com>
2023-07-05 10:28:49 +0800

    Update theme

--- client/src/app/theme.js
+++ client/src/app/theme.js
@@ -1,17 +1,25 @@
-import { createTheme } from "@mui/material/styles";
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#E42A41",
-      red: "#E42A41",
-      green: "#10924B",
-      text: "#FFFBE6",
-      yellow: "#F9BE34",
-      green2: "#17EB3F",
-      green3: "#72FFB2",
-    },
-  },
+let theme = createTheme({
+	typography: {
+		fontFamily: "Montserrat",
+	},
+	palette: {
+		primary: {
+			main: "#E42A41",
+			red: "#E42A41",
+			green: "#10924B",
+			text: "#FFFBE6",
+			yellow: "#F9BE34",
+			green2: "#17EB3F",
+			green3: "#72FFB2",
+		},
+		secondary: {
+			main: "#10924B",
+		},
+	},
 });
+
+theme = responsiveFontSizes(theme);
 
 export default theme;
