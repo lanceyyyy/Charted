238cc46ab2565280d370c9390abb1c4a8298fe10
Wyn Chrsitian <80365421+Wyn-Christian@users.noreply.github.com>
2023-07-09 22:34:25 +0800

    Add virtual image_url

--- server/models/product.js
+++ server/models/product.js
@@ -2,41 +2,40 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ProductSchema = new Schema(
-  {
-    name: String,
-    description: String,
-    image: String,
-    price: {
-      type: mongoose.Types.Decimal128,
-      get: getValue,
-    },
-    type: {
-      type: String,
-      enum: ["customizable", "fixed"],
-      required: true,
-    },
+	{
+		name: String,
+		description: String,
+		image: String,
+		price: {
+			type: mongoose.Types.Decimal128,
+			get: getValue,
+		},
 
-    size: String,
-    shape: String,
-    patrol_color: String,
-    patrol_light: String,
-    tassel_size: String,
+		size: String,
+		shape: String,
+		patrol_color: String,
+		patrol_light: String,
+		tassel_size: String,
 
-    availability: Boolean,
-    stocks: Number,
-  },
-  {
-    timestamps: true,
-    toJSON: {
-      getters: true,
-    },
-  }
+		availability: Boolean,
+		stocks: Number,
+	},
+	{
+		timestamps: true,
+		toJSON: {
+			getters: true,
+		},
+	}
 );
 function getValue(value) {
-  if (typeof value !== "undefined") {
-    return parseFloat(value.toString());
-  }
-  return value;
+	if (typeof value !== "undefined") {
+		return parseFloat(value.toString());
+	}
+	return value;
 }
+
+ProductSchema.virtual("image_url").get(function () {
+	return `/images/products/${this.image}`;
+});
 
 module.exports = mongoose.model("product", ProductSchema);
